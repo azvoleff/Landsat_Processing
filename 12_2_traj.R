868e247c792ca666696bf0c0b8977e6e761bfe96
Alex Zvoleff <azvoleff@gmail.com>
2014-08-11 19:28:20 -0700

    Streamline file input.

--- 12_2_traj.R
+++ 12_2_traj.R
@@ -31,6 +31,9 @@ out_dir <- file.path(prefix, 'Landsat', 'Composites', 'Change_Detection')
 
 chgmag_files <- c()
 zoi_files <- c()
+classes_1_filenames <- c()
+year_1s <- c()
+year_2s <- c()
 for (sitecode in sitecodes) {
     these_chgmag_files <- dir(chgdetect_dir,
                                pattern=paste0('^', sitecode, '_[0-9]{4}-[0-9]{4}_chgdetect_chgmag.tif$'),
@@ -44,15 +47,35 @@ for (sitecode in sitecodes) {
                          full.names=TRUE)
     stopifnot(length(this_zoi_file) == 1)
     
+    these_year_1s <- as.numeric(gsub('[_-]','', str_extract(these_chgmag_files, '_[0-9]{4}-')))
+    these_year_2s <- as.numeric(gsub('[_-]','', str_extract(these_chgmag_files, '-[0-9]{4}_')))
+    stopifnot(these_year_1s < these_year_2s)
+    year_1s <- c(year_1s, these_year_1s)
+    year_2s <- c(year_2s, these_year_2s)
+    
+    for (this_year_1 in these_year_1s) {
+        classes_1_filename <- dir(predictions_dir,
+                                  pattern=paste0(sitecode, '_mosaic_', this_year_1,
+                                                 '_predictors_predclasses.tif$'),
+                                  full.names=TRUE)
+        stopifnot(length(classes_1_filename) == 1)
+        classes_1_filenames <- c(classes_1_filenames, classes_1_filename)
+    }
+    
     chgmag_files <- c(chgmag_files, these_chgmag_files)
     
     zoi_files <- c(zoi_files, rep(this_zoi_file, length(these_chgmag_files)))
 }
 stopifnot(length(chgmag_files) == length(zoi_files))
+stopifnot(length(chgmag_files) == length(year_1s))
+stopifnot(length(chgmag_files) == length(year_2s))
+stopifnot(length(chgmag_files) == length(classes_1_filenames))
 
 # Run change detection on each pair
 notify(paste0('Starting change detection. ', length(chgmag_files), ' image sets to process.'))
 num_res <- foreach (chgmag_file=iter(chgmag_files), zoi_file=iter(zoi_files),
+                    classes_1_filename=iter(classes_1_filename),
+                    year_1=iter(year_1s), year_2=iter(year_2s),
                     .packages=c('teamlucc', 'notifyR', 'stringr', 'rgdal'),
                     .combine=c, .inorder=FALSE) %dopar% {
     raster_tmpdir <- file.path(temp, paste0('raster_',
@@ -62,9 +85,6 @@ num_res <- foreach (chgmag_file=iter(chgmag_files), zoi_file=iter(zoi_files),
 
     sitecode <- str_extract(basename(chgmag_file), '^[a-zA-Z]*')
 
-    year_1 <- as.numeric(gsub('[_-]','', str_extract(chgmag_file, '_[0-9]{4}-')))
-    year_2 <- as.numeric(gsub('[_-]','', str_extract(chgmag_file, '-[0-9]{4}_')))
-    stopifnot(year_1 < year_2)
 
     out_basename <- paste0(sitecode, '_', year_1, '-', year_2, '_chgdetect')
 
@@ -73,13 +93,6 @@ num_res <- foreach (chgmag_file=iter(chgmag_files), zoi_file=iter(zoi_files),
     if (length(output_files) >= 1 & !redo_chg_detection) {
         return()
     }
-
-    classes_1_filename <- dir(predictions_dir,
-                              pattern=paste0(sitecode, '_mosaic_', year_1,
-                                             '_predictors_predclasses.tif'),
-                              full.names=TRUE)
-    stopifnot(length(classes_1_filename) == 1)
-    classes_1_image <- stack(classes_1_filename)
 
     chgmag_image <- raster(chgmag_file)
 
@@ -106,7 +119,7 @@ num_res <- foreach (chgmag_file=iter(chgmag_files), zoi_file=iter(zoi_files),
               row.names=FALSE)
 
     if (chg_threshold < min_chg_threshold) chg_threshold <- min_chg_threshold
-    
+
     key_file_1 <- gsub('predclasses.tif', 'classeskey.csv', classes_1_filename)
     class_key <- read.csv(key_file_1)
     classnames <- class_key$class
